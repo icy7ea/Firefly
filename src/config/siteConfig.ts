7121263e750a63aa4e6652a8d5b8471f133ea56f
icy7ea <3097959868@qq.com>
2026-06-04 15:03:31 +0800

    博客内容

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,26 +7,25 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "冰红茶",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "icy7ea",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"个人博客网站",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
-		"Fuwari",
-		"Astro",
+		"Blog",
+		"CS",
+		"SE",
 		"ACGN",
 		"博客",
-		"技术博客",
 		"静态博客",
 	],
 
@@ -79,7 +78,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "icy7ea个人博客网站",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
